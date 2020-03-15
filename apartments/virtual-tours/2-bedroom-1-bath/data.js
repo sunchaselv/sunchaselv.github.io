748be21ca1080cb00257d2d40584b61a7953741c
Zelly Snyder <zelly@outlook.com>
2020-03-15 16:24:25 -0400

    Add all large file asets

---
+++ apartments/virtual-tours/2-bedroom-1-bath/data.js
@@ -0,0 +1,364 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dining-room",
+      "name": "dining room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.4959320960825284,
+        "pitch": 0.023525389131373586,
+        "fov": 1.39398230096323
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4469281734849453,
+          "pitch": 0.5033554459904366,
+          "rotation": 0,
+          "target": "7-living-room"
+        },
+        {
+          "yaw": 0.3243684072975537,
+          "pitch": 0.49378650270171676,
+          "rotation": 0,
+          "target": "6-kitchen"
+        },
+        {
+          "yaw": 2.700151059148137,
+          "pitch": 0.608201013948694,
+          "rotation": 0,
+          "target": "5-hallway"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.06723067814967898,
+          "pitch": 0.054470460872313,
+          "title": "Full-size washer and dryer<br>",
+          "text": "Every apartment comes with a full size washer and dryer.<br>"
+        }
+      ]
+    },
+    {
+      "id": "1-small-bedroom",
+      "name": "small bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.2819475960767335,
+        "pitch": 0.49179754167174394,
+        "fov": 1.39398230096323
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3533770780365426,
+          "pitch": 0.32324833629347083,
+          "rotation": 0,
+          "target": "5-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-walk-in-closet",
+      "name": "walk in closet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.008055365778435686,
+        "pitch": 0.24219055892066876,
+        "fov": 1.39398230096323
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.010706894633619513,
+          "pitch": 0.5153081439274221,
+          "rotation": 0,
+          "target": "3-master-bedroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-master-bedroom",
+      "name": "master bedroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.06993748440864778,
+        "pitch": 0.4381088564179709,
+        "fov": 1.39398230096323
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7403207998043762,
+          "pitch": 0.3536867799021124,
+          "rotation": 6.283185307179586,
+          "target": "2-walk-in-closet"
+        },
+        {
+          "yaw": -0.0024099842674232264,
+          "pitch": 0.3333077462478329,
+          "rotation": 0,
+          "target": "5-hallway"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-patio",
+      "name": "patio",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.10057542316249446,
+        "pitch": 0.048510490520628835,
+        "fov": 1.39398230096323
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7774237262448676,
+          "pitch": 0.44103236443061,
+          "rotation": 0,
+          "target": "7-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-hallway",
+      "name": "hallway",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.6015104969767986,
+        "pitch": 0.1870617538927135,
+        "fov": 1.39398230096323
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7792983648007521,
+          "pitch": 0.4176183560669813,
+          "rotation": 0,
+          "target": "3-master-bedroom"
+        },
+        {
+          "yaw": 1.5838719001665602,
+          "pitch": 0.5180608168155771,
+          "rotation": 0,
+          "target": "1-small-bedroom"
+        },
+        {
+          "yaw": -2.7652662610646814,
+          "pitch": 0.5101998674724051,
+          "rotation": 0,
+          "target": "0-dining-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-kitchen",
+      "name": "kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.7702951175692494,
+        "pitch": 0.40132814945541284,
+        "fov": 1.39398230096323
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.897171069635597,
+          "pitch": 0.5957828567000529,
+          "rotation": 5.497787143782138,
+          "target": "0-dining-room"
+        },
+        {
+          "yaw": -2.227841629832744,
+          "pitch": 0.45942962141212895,
+          "rotation": 0,
+          "target": "7-living-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-living-room",
+      "name": "living room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.7604178019765708,
+        "pitch": 0.42160460817971135,
+        "fov": 1.39398230096323
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.075343114655835,
+          "pitch": 0.36816887506277496,
+          "rotation": 0,
+          "target": "4-patio"
+        },
+        {
+          "yaw": -0.17332291805611888,
+          "pitch": 0.33915228475358816,
+          "rotation": 0,
+          "target": "0-dining-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
